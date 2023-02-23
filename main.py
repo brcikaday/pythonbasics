c630860ff590d478631a3ca4dd1408db991cd322
brcikaday <andrewstakyi30@gmail.com>
2023-02-23 17:15:03 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+Names_of_student=["andrews","micheal","godfred"]
+print(Names_of_student)
+second_student = Names_of_student[1]
+print(second_student)
+Names_of_student.append("fred")
+print(Names_of_student)
