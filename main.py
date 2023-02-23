535ea479a5859159ed8ab4b4727985780cd826b3
brcikaday <andrewstakyi30@gmail.com>
2023-02-23 17:22:58 +0000

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# creating a list
 Names_of_student=["andrews","micheal","godfred"]
 print(Names_of_student)
+#printing an item in a list
 second_student = Names_of_student[1]
 print(second_student)
+# adding an item to a list
 Names_of_student.append("fred")
 print(Names_of_student)
