e30386e7f77a73d50e579069b0af9fec80220524
brcikaday <andrewstakyi30@gmail.com>
2023-03-09 17:08:42 +0000

    updated file

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ Names_of_student.append("fred")
 print(Names_of_student)
 # how to push code to github 
 # git add . , git commit -m "", git push
+
+students_grade={}
+#adding to a dictionary
+students_grade["mike"]="A"
+print (students_grade)
+students_grade["prince"]="B"
+students_grade["andy"]="C"
+students_grade["alex"]="D"
+print(students_grade)
+students_grade.pop("alex")
+print(students_grade)
