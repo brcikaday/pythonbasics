d6f683b5ea1ee6128b4cdda17b7fc420b82e7602
brcikaday <andrewstakyi30@gmail.com>
2023-03-16 17:17:14 +0000

    my first assignment

---
+++ .vscode/assignments/assignment_1.py
@@ -0,0 +1,10 @@
+userdetails = []
+userinput = input("enter a name:")
+userdetails.append(userinput)
+userinput = input("enter age")
+userdetails.append(userinput)
+userinput = input("enter class:")
+userdetails.append(userinput)
+userinput = input("enter gender:")
+userdetails.append(userinput)
+print(f"your name is {userdetails[0]}, your agejeo is {userdetails[1]}, your class {userdetails[2]},your gender is {userdetails[3]}")
